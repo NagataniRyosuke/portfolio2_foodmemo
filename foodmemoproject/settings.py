1df4b4e85aa4e6b67bf128607abed9f0fe5d38cc
ryosuke <ryonsuke.n@gmail.com>
2024-09-16 22:26:27 +0900

    to comfirm

--- foodmemoproject/settings.py
+++ foodmemoproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@f-%n2va977s^co^&(w1wqaj4x26pr+xa3nk0t2)4d3)@rjjx^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['portfolio2-foodmemo.onrender.com', 'localhost', '127.0.0.1']
 
